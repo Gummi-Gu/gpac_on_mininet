f6b628667fa6049dd36b8de2bf9c71dbe6039b4c
GummiGu <Augustus0508@outlook.com>
2025-03-07 02:16:49 +0800

    延迟启动

--- Client/main.py
+++ Client/main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         src = fs.load_src(sys.argv[1])
     #otherwise load one of our DASH sequences
     else:
-        src = fs.load_src("http://127.0.0.1:10086/files/dash_tiled.mpd")
+        src = fs.load_src("http://192.168.16.184:10086/files/dash_tiled.mpd")
 
     # load our custom filter and assign its source
     my_filter = Factory.bufferFilter
