c7e382af46e868dc09336bbbae3f233c37a57acc
GummiGu <Augustus0508@outlook.com>
2025-04-22 10:15:10 +0800

    修改质量指导函数

--- Client/Interfaces.py
+++ Client/Interfaces.py
@@ -1,15 +1,23 @@
 import math
 import time
 
-import pyautogui
-import Factory
+
+import Client.Factory  as Factory
 
 
 class DashInterface:
     def __init__(self):
         self.srd_quantity = None
-
     def set_quality(self, slice_idx, srd_position=None):
+        """
+                根据鼠标位置设置切片的质量
+                :param slice_idx: 给定的切片序号（从0开始）
+                :param view_x: 鼠标的x坐标
+                :param view_y: 鼠标的y坐标
+                :return: 切片的质量
+        """
+        return Factory.videoSegmentStatus.get_quality_idx(slice_idx)
+    def set_quality_old(self, slice_idx, srd_position=None):
         """
         根据鼠标位置设置切片的质量
         :param slice_idx: 给定的切片序号（从0开始）
@@ -21,7 +29,7 @@ class DashInterface:
         #view_x, view_y = pyautogui.position()
         #view_x=view_x*1.6
         #view_y=view_y*1.6
-        view_x, view_y = Factory.viewpoint.get_view_position()
+        view_x, view_y = Factory.u,Factory.v
         #print(f"[Dash_Interface]视点在原图中的位置：({view_x}, {view_y})")
         if view_x is None or view_y is None:
             return 3
