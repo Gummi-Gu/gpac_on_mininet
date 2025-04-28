e8379c7c4e00dee46b6cb55504a4f1197fc9bf48
GummiGu <Augustus0508@outlook.com>
2025-04-29 00:19:05 +0800

    信息获取

--- mininet/main.py
+++ mininet/main.py
@@ -215,9 +215,10 @@ def setup_network():
         client1.cmd('cd /home/mininet/gpac_on_mininet/mininet && screen -dmS proxy1 python3 proxy.py client1')
         client2.cmd('cd /home/mininet/gpac_on_mininet/mininet && screen -dmS proxy2 python3 proxy.py client2')
         client3.cmd('cd /home/mininet/gpac_on_mininet/mininet && screen -dmS proxy3 python3 proxy.py client3')
+        print('server start')
         server.cmd('cd /home/mininet/gpac_on_mininet/mininet && screen -dmS monitor python3 monitor.py')
         server.cmd('cd /home/mininet/gpac_on_mininet/mininet && screen -dmS monitor1 python3 monitor2.py')
-        print('server start')
+        print('monitor start')
         TrafficControl.setup_tc(server)
 
         def get_eth1_ip(host):
